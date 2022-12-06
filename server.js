cd689408f38c2aee965b51393d75deedc72727b3
Ardit Demolli <n_arditi_98@hotmail.com>
2022-12-06 14:53:20 +0100

    Error 404 page - Added

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const contactController = require('./controllers/contactController');
-const homeController = require('./controllers/homeController');
-const newsletterController = require('./controllers/newsletterController');
-const productsController = require('./controllers/productsController');
-const PORT = 8000
+const contactController = require("./controllers/contactController");
+const homeController = require("./controllers/homeController");
+const newsletterController = require("./controllers/newsletterController");
+const productsController = require("./controllers/productsController");
+const errorController = require("./controllers/errorController");
+const PORT = 8000;
 
 app.use(express.static("public"));
 
-app.get('/', homeController.getHomeView)
-app.get('/contact', contactController.getContactView)
-app.get('/newsletter', newsletterController.getNewsletterView)
+app.get("/", homeController.getHomeView);
+app.get("/contact", contactController.getContactView);
+app.get("/newsletter", newsletterController.getNewsletterView);
 // app.get('/products', productsController.getProductsView)
 app.use("/products", productsController);
 
+app.use(errorController.getErrorView);
 
 app.listen(PORT, () => {
-    console.log(`Server is running one port: ${PORT}`)
-})
+  console.log(`Server is running one port: ${PORT}`);
+});
